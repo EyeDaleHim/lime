afced4e44fd03d17188913b74eca9e1225c97e18
Karim Akra <karimakra512@gmail.com>
2025-10-10 20:05:38 +0300

    Fix the audio stopping after disconneting the audio device

--- src/lime/media/AudioManager.hx
+++ src/lime/media/AudioManager.hx
@@ -6,6 +6,7 @@ import lime.media.openal.AL;
 import lime.media.openal.ALC;
 import lime.media.openal.ALContext;
 import lime.media.openal.ALDevice;
+import lime.app.Application;
 #if (js && html5)
 import js.Browser;
 #end
@@ -18,6 +19,9 @@ import js.Browser;
 class AudioManager
 {
 	public static var context:AudioContext;
+
+	@:noCompletion
+	private static var alDeviceName:String = "";
 
 	public static function init(context:AudioContext = null)
 	{
@@ -33,12 +37,15 @@ class AudioManager
 				{
 					var alc = context.openal;
 
-					var device = alc.openDevice();
-					if (device != null)
+					alc.makeContextCurrent(ctx);
+					alc.processContext(ctx);
+
+					if (Application.current != null && ALC.isExtensionPresent(device, 'ALC_EXT_disconnect'))
 					{
-						var ctx = alc.createContext(device);
-						alc.makeContextCurrent(ctx);
-						alc.processContext(ctx);
+						if (!Application.current.onUpdate.has(checkDevice))
+						{
+							Application.current.onUpdate.add(checkDevice);
+						}
 					}
 				}
 				#end
@@ -69,6 +76,14 @@ class AudioManager
 				var device = alc.getContextsDevice(currentContext);
 				alc.resumeDevice(device);
 				alc.processContext(currentContext);
+
+				if (Application.current != null && ALC.isExtensionPresent(device, 'ALC_EXT_disconnect'))
+				{
+					if (!Application.current.onUpdate.has(checkDevice))
+					{
+						Application.current.onUpdate.add(checkDevice);
+					}
+				}
 			}
 		}
 		#end
@@ -81,10 +96,18 @@ class AudioManager
 		{
 			var alc = context.openal;
 			var currentContext = alc.getCurrentContext();
+			var device = alc.getContextsDevice(currentContext);
 
 			if (currentContext != null)
 			{
-				var device = alc.getContextsDevice(currentContext);
+				if (Application.current != null && ALC.isExtensionPresent(device, 'ALC_EXT_disconnect'))
+				{
+					if (Application.current.onUpdate.has(checkDevice))
+					{
+						Application.current.onUpdate.remove(checkDevice);
+					}
+				}
+
 				alc.makeContextCurrent(null);
 				alc.destroyContext(currentContext);
 
@@ -106,11 +129,19 @@ class AudioManager
 		{
 			var alc = context.openal;
 			var currentContext = alc.getCurrentContext();
+			var device = alc.getContextsDevice(currentContext);
 
 			if (currentContext != null)
 			{
+				if (Application.current != null && ALC.isExtensionPresent(device, 'ALC_EXT_disconnect'))
+				{
+					if (Application.current.onUpdate.has(checkDevice))
+					{
+						Application.current.onUpdate.remove(checkDevice);
+					}
+				}
+
 				alc.suspendContext(currentContext);
-				var device = alc.getContextsDevice(currentContext);
 
 				if (device != null)
 				{
@@ -118,6 +149,66 @@ class AudioManager
 				}
 			}
 		}
+		#end
+	}
+
+	private static function checkDevice(_):Void
+	{
+		var alc = context.openal;
+		var currentContext = alc.getCurrentContext();
+		var alDevice = alc.getContextsDevice(currentContext);
+
+		var curDeviceName = ALC.getString(null, ALC.ALL_DEVICES_SPECIFIER);
+    if (curDeviceName != null && curDeviceName != alDeviceName)
+    {
+    	trace('[OPENAL] Lost audio device "$alDeviceName", reopening...');
+      if (ALC.reOpenDevice(alDevice, null, null))
+      {
+      	alDeviceName = curDeviceName;
+      	trace('[OPENAL] Audio device reopened on "$alDeviceName"!');
+      }
+    }
+	}
+
+	@:noCompletion
+	private static function setupConfig():Void
+	{
+		#if (lime_openal && (windows || mac || linux || android))
+		final alConfig:Array<String> = [];
+
+		alConfig.push('[general]');
+		alConfig.push('channels=stereo');
+		alConfig.push('sample-type=float32');
+		alConfig.push('stereo-mode=speakers');
+		alConfig.push('stereo-encoding=panpot');
+		alConfig.push('hrtf=false');
+		alConfig.push('cf_level=0');
+		alConfig.push('resampler=fast_bsinc24');
+		alConfig.push('front-stablizer=false');
+		alConfig.push('output-limiter=false');
+		alConfig.push('volume-adjust=0');
+		alConfig.push('period_size=441');
+
+		alConfig.push('[decoder]');
+		alConfig.push('hq-mode=false');
+		alConfig.push('distance-comp=false');
+		alConfig.push('nfc=false');
+
+		try
+		{
+			final directory:String = Path.directory(Path.withoutExtension(System.applicationStorageDirectory));
+			final path:String = Path.join([directory, #if windows 'audio-config.ini' #else 'audio-config.conf' #end]);
+			final content:String = alConfig.join('\n');
+
+			if (!FileSystem.exists(directory))
+				FileSystem.createDirectory(directory);
+
+			if (!FileSystem.exists(path))
+				File.saveContent(path, content);
+
+			Sys.putEnv('ALSOFT_CONF', path);
+		}
+		catch (e:Dynamic) {}
 		#end
 	}
 }
